fd94b157ceb4f40800e918c3615d6557a392008f
Md. Solayman <mdsolaymannuhin@gmail.com>
2021-02-22 23:31:45 +0600

    dynamicComponents

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Details of students</h1>
+      <FirstComponent name="Rahim" expertise="Singing"></FirstComponent>
+      <FirstComponent name="Karim" expertise="Dancing"></FirstComponent>
+      <FirstComponent name="Rabiul" expertise="Cricket"></FirstComponent>
     </div>
   );
 }
-
+function FirstComponent(props) {
+  const myStyle = {
+    border: "2px solid red",
+    padding: "10px",
+    width: "50%",
+    margin: "10px auto",
+  };
+  return (
+    <div style={myStyle}>
+      <h2>My Name is {props.name} </h2>
+      <h3>I am expert in {props.expertise}</h3>
+    </div>
+  );
+}
 export default App;
